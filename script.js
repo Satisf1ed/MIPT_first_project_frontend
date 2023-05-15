3b236c24f9ba62e19b831c814e1654cdc36ce542
Satisf1ed <zhogov.andrey.d@gmail.com>
2023-05-15 23:22:18 +0300

    sending form to the server

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const fullImgBox = document.getElementById("fullImgBox");
 const fullImg = document.getElementById("fullImg");
 const form = document.getElementById('write-me');
+const buttonSubmitForm = document.querySelector('.form__submit');
 const buttonChangeTheme = document.querySelector('.home-darkness__navbar_change-theme');
 const buttonRain = document.querySelector('.button_rain');
 let rain = document.querySelector('.rain');
 let slideIndex = 1;
 const allInput = document.querySelectorAll('.form__input');
-let isLightTheme = true;
 
 function removeError(input) {
     const parent = input.parentNode;
@@ -55,14 +55,32 @@ function validation() {
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
     if (validation()) {
-        fetch('', {
+        buttonSubmitForm.classList.add('form__submit_sending');
+        buttonSubmitForm.textContent = 'Sending...';
+        const formName = document.getElementById('name').value;
+        const formEmail = document.getElementById('email').value;
+        const formText = document.getElementById('textarea').value;
+        console.log(formName, formEmail, formText);
+        let response = fetch('http://localhost:3000', {
             method: 'POST',
-            body: {
-            }
+            mode:'no-cors',
+            headers: {
+                'Content-Type': 'charset=UTF-8'
+            },
+            body: JSON.stringify({
+                name: formName,
+                email: formEmail,
+                text: formText
+            })
         });
-        form.reset();
+        if (response.ok) {
+            buttonSubmitForm.classList.add('form__submit_success');
+            buttonSubmitForm.textContent = 'Sent!';
+        } else {
+            alert('Oops... Something went wrong :(');
+            closeForm();
+        }
     }
 });
 
@@ -75,6 +93,13 @@ function closeForm() {
     for (const input of allInput) {
         removeError(input);
     }
+    if (buttonSubmitForm.classList.contains('form__submit_sending')) {
+        buttonSubmitForm.classList.remove('form__submit_sending');
+    }
+    if (buttonSubmitForm.classList.contains('form__submit_success')) {
+        buttonSubmitForm.classList.remove('form__submit_success');
+    }
+    buttonSubmitForm.textContent = 'Send';
     form.reset();
 }
 
